86b8ffd6e3251d52ef9ef5a8fc7a7690e43b69fe
Makkintoshia <85276507+Makkintoshia@users.noreply.github.com>
2023-07-10 12:52:05 +0300

    first commit

---
+++ Test Space/Assets/Space Space/Scripts/Enemy.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Enemy : MonoBehaviour {
+
+    #region FIELDS
+    [Tooltip("Health points in integer")]
+    public int health;
+
+    [Tooltip("Enemy's projectile prefab")]
+    public GameObject Projectile;
+
+    [Tooltip("VFX prefab generating after destruction")]
+    public GameObject destructionVFX;
+    public GameObject hitEffect;
+    
+    [HideInInspector] public int shotChance; //probability of 'Enemy's' shooting during tha path
+    [HideInInspector] public float shotTimeMin, shotTimeMax; //max and min time for shooting from the beginning of the path
+    #endregion
+
+    private void Start()
+    {
+        Invoke("ActivateShooting", Random.Range(shotTimeMin, shotTimeMax));
+    }
+
+    //coroutine making a shot
+    void ActivateShooting() 
+    {
+        if (Random.value < (float)shotChance / 100)                           
+        {                         
+            Instantiate(Projectile,  gameObject.transform.position, Quaternion.identity);             
+        }
+    }
+
+   
+    public void GetDamage(int damage) 
+    {
+        health -= damage;          
+        if (health <= 0)
+            Destruction();
+        else
+            Instantiate(hitEffect,transform.position,Quaternion.identity,transform);
+    }    
+
+   
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            if (Projectile.GetComponent<Projectile>() != null)
+                Player.instance.GetDamage(Projectile.GetComponent<Projectile>().damage);
+            else
+                Player.instance.GetDamage(1);
+        }
+    }
+
+  
+    void Destruction()                           
+    {        
+        Instantiate(destructionVFX, transform.position, Quaternion.identity); 
+        Destroy(gameObject);
+    }
+}
